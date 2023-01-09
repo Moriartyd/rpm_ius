7beed52297c2e00cd43b40458d3ad067a54088c5
Moriartyd <dima@galeev.ru>
2023-01-10 00:43:20 +0300

    Очистил репу от мусора, исправил дизайн

--- src/main/java/ru/galeev/labone/HelloApplication.java
+++ src/main/java/ru/galeev/labone/HelloApplication.java
@@ -11,7 +11,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 530, 206);
+        Scene scene = new Scene(fxmlLoader.load(), 728, 145);
         stage.setTitle("Лабораторная работа №1");
         stage.setScene(scene);
         stage.show();
